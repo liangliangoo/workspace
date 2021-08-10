73be52e84323a0fa11dc81f8be95204fbc5e9906
xiongliang <1640432919@qq.com>
2021-08-10 22:13:12 +0800

    <ADD>BATCHDemo

---
+++ batchDemo/src/main/java/com/example/batchdemo/BatchDemoApplication.java
@@ -0,0 +1,21 @@
+package com.example.batchdemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+@RestController
+public class BatchDemoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BatchDemoApplication.class, args);
+    }
+
+    @RequestMapping("/")
+    public String success(){
+        return "SUCCESS";
+    }
+
+}
