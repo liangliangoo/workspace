6fb1e99f9184dc906cb4c79293f422057add1f73
xiongliang <1640432919@qq.com>
2021-08-22 11:31:57 +0800

    <ADD>111

--- springboot/src/main/java/com/xiaoxiong/springboot/SpringbootApplication.java
+++ springboot/src/main/java/com/xiaoxiong/springboot/SpringbootApplication.java
@@ -2,8 +2,12 @@ package com.xiaoxiong.springboot;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+import org.springframework.web.filter.CorsFilter;
 
 @RestController
 @SpringBootApplication
@@ -14,12 +18,31 @@ public class SpringbootApplication {
     }
 
     /**
-     * 为LBS能够正常访问
+     * 为了SLB检查可以正常返回，web项目默认访问/
      * @return string
      */
     @RequestMapping("/")
     public String success(){
         return "Success";
     }
+
+    /**
+     * 默认服务端支持跨域
+     * @return CorsFilter
+     */
+    @Bean
+    public CorsFilter corsFilter(){
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        CorsConfiguration corsConfiguration = new CorsConfiguration();
+        Long maxAge = new Long(86400);
+        corsConfiguration.setMaxAge(maxAge);
+        corsConfiguration.addAllowedHeader("*");
+        corsConfiguration.addAllowedOrigin("*");
+        corsConfiguration.addAllowedMethod("*");
+        source.registerCorsConfiguration("/**",corsConfiguration);
+        return new CorsFilter(source);
+    }
+
+
 
 }
