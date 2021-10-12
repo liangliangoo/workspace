e0515b72fc37157bc8f3a5bcc3bf514541fa6cf8
熊亮 <1640432919@qq.com>
2021-10-12 19:07:00 +0800

    <feat>(<zkCurator>):init auth2.0 demo

--- auth2Demo/src/main/java/com/example/auth2demo/Auth2DemoApplication.java
+++ auth2Demo/src/main/java/com/example/auth2demo/Auth2DemoApplication.java
@@ -3,6 +3,9 @@ package com.example.auth2demo;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+/**
+ * @author xiongliang
+ */
 @SpringBootApplication
 public class Auth2DemoApplication {
 
