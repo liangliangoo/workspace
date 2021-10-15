ed5e4c144ea5315c4f3296056ca4fb2f3e0c9379
熊亮 <1640432919@qq.com>
2021-10-15 10:33:31 +0800

    <feat>(auth2Demo): simple oauth2.0 demo

--- auth2Demo/src/main/java/com/xiaoxiong/auth2demo/controller/TestController.java
+++ auth2Demo/src/main/java/com/xiaoxiong/auth2demo/controller/TestController.java
@@ -1,10 +1,6 @@
 package com.xiaoxiong.auth2demo.controller;
 
 import org.springframework.web.bind.annotation.GetMapping;
-<<<<<<< HEAD
-=======
-import org.springframework.web.bind.annotation.RequestMapping;
->>>>>>> origin/feature/oauth2.0-2021-10-10
 import org.springframework.web.bind.annotation.RestController;
 
 /**
@@ -13,10 +9,6 @@ import org.springframework.web.bind.annotation.RestController;
  * @since 2021/10/14  16:45
  */
 @RestController
-<<<<<<< HEAD
-=======
-@RequestMapping("/")
->>>>>>> origin/feature/oauth2.0-2021-10-10
 public class TestController {
 
     @GetMapping("/callback")
