5a46c756e464cba4deaa17620f154e00db12e06e
熊亮 <1640432919@qq.com>
2021-10-23 14:07:01 +0800

    <feat>(designPatternApp):implement static proxy

---
+++ designPatternApp/src/main/java/com/xiaoxiong/design/proxy/staticproxy/package-info.java
@@ -0,0 +1,4 @@
+package com.xiaoxiong.design.proxy.staticproxy;
+/**
+ * 静态代理
+ */
--- dubbo/dubbo-spi/src/main/java/com/xiaoxiong/dubbospi/aop/service/impl/HelloServiceWrapper.java
+++ dubbo/dubbo-spi/src/main/java/com/xiaoxiong/dubbospi/aop/service/impl/HelloServiceWrapper.java
@@ -3,6 +3,8 @@ package com.xiaoxiong.dubbospi.aop.service.impl;
 import com.xiaoxiong.dubbospi.aop.service.HelloService;
 
 /**
+ * 这个包装类就是dubbo spi AOP的实现
+ * 但是这个包装类必须在 /META-INF/services/com.xiaoxiong.dubbospi.aop.service.HelloService中说明
  * @author xiongliang
  * @version 1.0
  * @since 2021/10/22  22:23
