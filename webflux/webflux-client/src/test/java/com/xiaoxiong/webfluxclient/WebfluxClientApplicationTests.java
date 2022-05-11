82aa63d69dae98febb55be418a73d3e317bbabcc
熊亮 <1640432919@qq.com>
2022-05-11 19:25:09 +0800

    <build>(webflux-client): build webflux client

---
+++ webflux/webflux-client/src/test/java/com/xiaoxiong/webfluxclient/WebfluxClientApplicationTests.java
@@ -0,0 +1,13 @@
+package com.xiaoxiong.webfluxclient;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WebfluxClientApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
