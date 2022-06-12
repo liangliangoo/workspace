7076cde9e69a0a5e88ad20b490a30c2061c97f1e
熊亮 <1640432919@qq.com>
2022-06-12 20:55:03 +0800

    springboot配置文件激活ç仅仅是对application.properties中属性的ä¸覆盖

--- springboot/src/main/java/com/xiaoxiong/springboot/demo/TestController.java
+++ springboot/src/main/java/com/xiaoxiong/springboot/demo/TestController.java
@@ -1,6 +1,7 @@
 package com.xiaoxiong.springboot.demo;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -20,6 +21,18 @@ public class TestController {
     @GetMapping("show")
     public String test_show(String data) {
         return basicDemo.process(data);
+    }
+
+    @Value("${spring.application.name:applicaition-properties}")
+    private String applicationName;
+    @Value("${application-test:applicaition-profile}")
+    private String profile;
+
+
+    @GetMapping("applicationName")
+    public String getApplicationName() {
+        System.out.println(profile);
+        return applicationName;
     }
 
 }
