2d9cca6b7d45d6e6b96b5060bb0031fb512f5e62
六月 <liuyue@yizhoucp.cn>
2022-07-28 22:17:17 +0800

    build biz project

---
+++ biz/README.md
@@ -0,0 +1,12 @@
+# 业务相关的案例
+
+## 红包发放
+
+#### 开红包流程图
+
+<img src="https://www.processon.com/view/link/62e29572e0b34d06e56b93fd">
+
+## 七夕抽奖活动
+
+
+
---
+++ biz/src/test/java/com/xiaoxiong/biz/biz/BizApplicationTests.java
@@ -0,0 +1,13 @@
+package com.xiaoxiong.biz.biz;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BizApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
