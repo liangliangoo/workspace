ed81eb828a2c35755f23a41fa5896720aea02953
熊亮 <1640432919@qq.com>
2022-09-05 22:48:49 +0800

    <feat>(mission-services): 任务推进 抽象1

---
+++ biz/mission-services/src/main/java/com/xiaoxiong/mission/task/handler/CheckerContext.java
@@ -0,0 +1,17 @@
+package com.xiaoxiong.mission.task.handler;
+
+import lombok.Data;
+
+/**
+ * @author xiongliang
+ * @version 1.0
+ * @description 任务校验上下文
+ * @since 2022/9/5  22:37
+ */
+@Data
+public class CheckerContext {
+
+
+
+
+}
---
+++ biz/mission-services/src/main/java/com/xiaoxiong/mission/task/handler/CheckerHandler.java
@@ -0,0 +1,13 @@
+package com.xiaoxiong.mission.task.handler;
+
+/**
+ * @author xiongliang
+ * @version 1.0
+ * @description 任务检查handler
+ * @since 2022/9/5  22:36
+ */
+public interface CheckerHandler {
+
+
+
+}
