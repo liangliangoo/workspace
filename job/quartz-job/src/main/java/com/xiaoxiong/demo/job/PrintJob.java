be3b2a5672237fed8f08ba8a2c910ff53c8810a2
六月 <liuyue@yizhoucp.cn>
2023-03-01 19:58:49 +0800

    fix(job): 处理日志不打印问题

--- job/quartz-job/src/main/java/com/xiaoxiong/demo/job/PrintJob.java
+++ job/quartz-job/src/main/java/com/xiaoxiong/demo/job/PrintJob.java
@@ -1,22 +1,27 @@
 package com.xiaoxiong.demo.job;
 
 import com.alibaba.fastjson.JSON;
+import lombok.extern.slf4j.Slf4j;
 import org.quartz.Job;
 import org.quartz.JobExecutionContext;
-import org.quartz.JobExecutionException;
 
 /**
  * @Author 六月
  * @Date 2023/3/1 17:29
  * @Version 1.0
  */
+@Slf4j
 public class PrintJob implements Job {
 
 
     @Override
-    public void execute(JobExecutionContext jobExecutionContext) throws JobExecutionException {
-        System.out.println(JSON.toJSON(jobExecutionContext));
-        System.out.println(jobExecutionContext.getJobDetail().hashCode());
+    public void execute(JobExecutionContext jobExecutionContext) {
+        try {
+            log.debug("execute context {}",JSON.toJSONString(jobExecutionContext.getJobDetail()));
+            log.debug("jobDetail hashCode {}", jobExecutionContext.getJobDetail().hashCode());
+        } catch (Exception e) {
+            log.debug("PrintJob error {}", e);
+        }
     }
 
 }
