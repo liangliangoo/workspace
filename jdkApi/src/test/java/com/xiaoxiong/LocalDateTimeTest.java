30714b3ac3573dcabba9ceb68170e882095ee0e6
六月 <liuyue@yizhoucp.cn>
2023-04-06 20:00:02 +0800

    <feat>(jdk): LocalDateTimeTest

---
+++ jdkApi/src/test/java/com/xiaoxiong/LocalDateTimeTest.java
@@ -0,0 +1,20 @@
+package com.xiaoxiong;
+
+import org.junit.jupiter.api.Test;
+
+import java.time.LocalDateTime;
+
+/**
+ * @Author 六月
+ * @Date 2023/4/6 12:27
+ * @Version 1.0
+ */
+public class LocalDateTimeTest {
+
+    @Test
+    void test_isAfter() {
+        LocalDateTime parse = LocalDateTime.parse("2023-04-01T20:00:00");
+        System.out.println(parse.isAfter(LocalDateTime.now()));
+    }
+
+}
