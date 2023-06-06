d71769e3a6c4b82b57c741c656126cf500d0f892
六月 <liuyue@yizhoucp.cn>
2023-06-06 19:48:15 +0800

    <feat>:  map api

---
+++ jdk-api/src/test/java/com/xiaoxiong/jdk/map/MapTest.java
@@ -0,0 +1,26 @@
+package com.xiaoxiong.jdk.map;
+
+import com.alibaba.fastjson.JSON;
+import com.google.common.collect.Maps;
+import org.junit.jupiter.api.Test;
+
+import java.util.HashMap;
+
+/**
+ * @Author 六月
+ * @Date 2023/6/6 18:30
+ * @Version 1.0
+ */
+public class MapTest {
+
+    @Test
+    public void test_putAll() {
+        HashMap<String, Integer> result1 = Maps.newHashMap();
+        result1.put("1", 111);
+        HashMap<String, Integer> result2 = Maps.newHashMap();
+        result2.put("1", 222);
+        result1.putAll(result2);
+        System.out.println(JSON.toJSONString(result1));
+    }
+
+}
