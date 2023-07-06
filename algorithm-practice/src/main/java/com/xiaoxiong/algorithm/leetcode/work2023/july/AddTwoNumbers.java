098256ec46dcf7f6cd040e4882d71bc1603b4848
小熊不会写代码 <1640432919@qq.com>
2023-07-06 22:43:48 +0800

    <feat>: 两数之和

---
+++ algorithm-practice/src/main/java/com/xiaoxiong/algorithm/leetcode/work2023/july/AddTwoNumbers.java
@@ -0,0 +1,48 @@
+package com.xiaoxiong.algorithm.leetcode.work2023.july;
+
+/**
+ * <a href="https://leetcode.cn/problems/add-two-numbers/solution/liang-shu-xiang-jia-by-leetcode-solution/"></a>
+ * @author xiongliang
+ * @version 1.0
+ * @description 两数相加
+ * @since 2023/7/6  22:34
+ */
+public class AddTwoNumbers {
+    public ListNode addTwoNumbers(ListNode l1, ListNode l2) {
+        ListNode head = null, tail = null;
+        int carry = 0;
+        while (l1 != null || l2 != null) {
+            int n1 = l1 == null ? 0 : l1.val;
+            int n2 = l2 == null ? 0 : l2.val;
+            int sum = n1 + n2 + carry;
+            if (head == null) {
+                head = tail = new ListNode(sum % 10);
+            } else {
+                tail.next = new ListNode(sum % 10);
+                tail = tail.next;
+            }
+            carry = sum / 10;
+            if (l1 != null) {
+                l1 = l1.next;
+            }
+            if (l2 != null) {
+                l2 = l2.next;
+            }
+        }
+        if (carry > 0) {
+            tail.next = new ListNode(carry);
+        }
+        return head;
+    }
+}
+
+
+// Definition for singly-linked list.
+class ListNode {
+    int val;
+    ListNode next;
+    ListNode() {}
+    ListNode(int val) { this.val = val; }
+    ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+}
+
