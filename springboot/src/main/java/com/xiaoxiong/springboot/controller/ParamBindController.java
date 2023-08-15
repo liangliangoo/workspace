515344c8aadabce77109320cb0082dd39d020455
六月 <liuyue@yizhoucp.cn>
2023-08-15 16:07:40 +0800

    <feat>:  mvn 接口参数

--- springboot/src/main/java/com/xiaoxiong/springboot/controller/ParamBindController.java
+++ springboot/src/main/java/com/xiaoxiong/springboot/controller/ParamBindController.java
@@ -1,22 +1,24 @@
 package com.xiaoxiong.springboot.controller;
 
 import com.alibaba.fastjson2.JSON;
+import com.alibaba.fastjson2.JSONObject;
 import com.xiaoxiong.springboot.param.ParamBind;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.autoconfigure.http.HttpMessageConverters;
 import org.springframework.context.ApplicationContext;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import javax.annotation.Resource;
 
 /**
  * 参数绑定处理
+ *
  * @Author 六月
  * @Date 2023/4/26 10:46
  * @Version 1.0
  */
 @Slf4j
+@CrossOrigin
 @RestController
 public class ParamBindController {
 
@@ -31,4 +33,9 @@ public class ParamBindController {
         return JSON.toJSONString(paramBind);
     }
 
+    @PostMapping("/api/mvc/post-request-params")
+    public String postRequestParams(@RequestParam(name = "type", required = false) String type, @RequestBody JSONObject jsonObject) {
+        log.info("postRequestParams type {} params {}", type, jsonObject.toJSONString());
+        return jsonObject.toJSONString();
+    }
 }
