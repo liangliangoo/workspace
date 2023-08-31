4d5210f56eb90bc300c7fcbd393bf566fff7f3ba
六月 <liuyue@yizhoucp.cn>
2023-08-31 12:24:56 +0800

    feat:  日期处理

--- jdk-api/src/test/java/com/xiaoxiong/Test01.java
+++ jdk-api/src/test/java/com/xiaoxiong/Test01.java
@@ -1,7 +1,14 @@
 package com.xiaoxiong;
 
 import cn.hutool.core.util.StrUtil;
+import com.google.common.collect.Lists;
 import lombok.Data;
+import org.apache.commons.lang3.StringUtils;
+
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+import java.util.ArrayList;
+import java.util.List;
 
 /**
  * 基础数据类型
@@ -20,6 +27,23 @@ public class Test01 {
         System.out.println(StrUtil.concat(true, null, null));
         System.out.println(StrUtil.concat(false, "sss", null));
         System.out.println(StrUtil.join("-", "1", null, "2"));
+        List<String> strings = parseBatchDateParams("20230801@20230831");
+        System.out.println(strings.size());
+    }
+
+    private static List<String> parseBatchDateParams(String params) {
+        if (StringUtils.isBlank(params) || params.split("@").length < 2) {
+            return Lists.newArrayList();
+        }
+        String[] split = params.split("@");
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyyMMdd");
+        LocalDate start = LocalDate.parse(split[0], formatter);
+        LocalDate end = LocalDate.parse(split[1], formatter);
+        ArrayList<String> ret = Lists.newArrayList();
+        for (LocalDate i = start; i.isBefore(end) || i.isEqual(end); i = i.plusDays(1)) {
+            ret.add(String.format("%s-%s", formatter.format(i), formatter.format(i)));
+        }
+        return ret;
     }
 
     @Data
